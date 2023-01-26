abbb17cd9141f4149efa50982f9b52f32b2ca184
nicolaspl96 <polonicolas96.com>
2023-01-26 12:56:51 +0000

    practica2

---
+++ 05_tuples.py
@@ -0,0 +1,15 @@
+### tuples
+
+my_tuple =tuple()
+my_other_tuple = ()
+
+my_tuple = (26, 1.7, "polo", "nicolas", "polo")
+
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])
+print(my_tuple[-1])
+
+print(my_tuple.count("polo"))
+print(my_tuple.index("nicolas"))
---
+++ 06_step.py
@@ -0,0 +1,14 @@
+### sets
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set))
+
+my_other_set = {"nicolas", "polo", 26}
+print(type(my_other_set))
+
+print(len(my_other_set))
+
+
